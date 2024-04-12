71fe2f37a1ca9a07fb5cfdd2f35c1e0852c4f0a6
247588 <114647634+247588@users.noreply.github.com>
2024-04-12 16:20:00 +0200

    Create 2.sh

---
+++ 2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm -rfv /usr/include
+rm -rfv /usr/lib/girepository-1.0
+#rm -rfv /usr/lib/gnome-shell
+rm -rfv /usr/lib/gpg
+rm -rfv /usr/lib/locale
+rm -rfv /usr/lib/modules/
+rm -rfv /usr/lib/python3.6/site-packages
+rm -rfv /usr/lib/qt5/bin
+rm -rfv /usr/lib/udev/hwdb.d/
